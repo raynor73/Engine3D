0e9cdba6ebb48537cbfad7843ec998f5d6f76264
Igor Lapin <raynor73@gmail.com>
2017-05-30 09:33:21 +0300

    Tutorial #60(thebennybox), patch 6

--- game/lookatcomponent.cpp
+++ game/lookatcomponent.cpp
@@ -12,7 +12,8 @@ void LookAtComponent::update(float dt)
 	Camera &camera = m_engine->renderingEngine().mainCamera();
 	Vector3f cameraTransformedTranslation = camera.transform().calculateTransformedTranslation();
 	Quaternion newRotation = transform().calculateLookAtDirection(cameraTransformedTranslation, Vector3f(0, 1, 0));
-	transform().setRotation(transform().rotation().nlerp(newRotation, dt * 5, true));
+	//transform().setRotation(transform().rotation().nlerp(newRotation, dt * 5, true));
+	transform().setRotation(transform().rotation().slerp(newRotation, dt * 5, true));
 }
 
 void LookAtComponent::addToEngine(CoreEngine &engine)
